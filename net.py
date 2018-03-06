fa1b86d5653082b8d3dcab6eb4a91c73c3b2a41f
Iron Mastiff <1760298283@qq.com>
2018-03-07 00:32:37 +0800

    Created the net

---
+++ net.py
@@ -0,0 +1,11 @@
+import pickle as pkl
+import time
+import os
+
+import matplotlib.pyplot as plot
+from scipy.io import matlab
+import tensorflow as tf
+import numpy as np
+
+import data_preprocessor
+import utils
