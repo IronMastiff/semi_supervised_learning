370832157589a8bb3b7b39f04053c763c2872f28
Iron Mastiff <1760298283@qq.com>
2018-03-06 13:36:46 +0800

    Add hook

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pickle as pkl
 import time
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,4 +15,22 @@ from tqdm import tqdm    # 用包装好的方法实现进度条
 
 data_dir = 'data'
 
-# if not isdir( data_dir ):
+if not isdir( data_dir ):
+    os.makedirs( data_dir )
+
+class DLProgress( tqdm ):
+    last_block = 0
+
+    def hook( self, block_num = 1, block_size = 1, total_size = None ):
+        self.total = total_size
+        self.updata( ( block_num - self.last_block ) * block_size )
+        self.last_block = block_num
+
+
+if not isfile( data_dir + 'train_32x32.mat' ):
+    with DLProgress( unit = 'B', unit_scale = True, miniters = 1, desc = 'SVHN Training Set' ) as pbar:    # ????
+        urlretrieve(
+            'http://ufldl.stanford.edu/housenumbers/train_32x32.amt',
+            data_dir + 'train_32x32.mat',
+            pbar.hook
+        )
