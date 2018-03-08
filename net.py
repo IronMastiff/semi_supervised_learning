0f9358439d6c84dfeff662f88b8c1426ca555e9e
Iron Mastiff <1760298283@qq.com>
2018-03-08 15:16:57 +0800

    Add model_inputs

--- net.py
+++ net.py
@@ -9,3 +9,11 @@ import numpy as np
 
 import data_preprocessor
 import utils
+
+def model_inputs( real_dim, z_dim ):
+    inputs_real = tf.placeholder( tf.float32, ( None, *real_dim ), name = 'input_real' )
+    inputs_z = tf.placeholder( tf.float32, ( None, z_dim ), name = 'input_z' )
+    y = tf.placeholder( tf.float32, ( None ), name = 'y' )
+    label_mask = tf.placeholder( tf.float32, ( None ), name = 'label_mask' )
+
+    return input_real, input_z, y, label_mask
