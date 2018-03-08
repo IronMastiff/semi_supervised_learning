20af55287e214495ee22b504d7c8d2e76ef3f90b
Iron Mastiff <1760298283@qq.com>
2018-03-08 15:41:41 +0800

    Add generator function

--- net.py
+++ net.py
@@ -17,3 +17,27 @@ def model_inputs( real_dim, z_dim ):
     label_mask = tf.placeholder( tf.float32, ( None ), name = 'label_mask' )
 
     return input_real, input_z, y, label_mask
+
+def generator( z, output_dim, reuse = False, alpha = 0.2, training = True, size_mult = 128 ):
+    with tf.variable_scope( 'generator', reuse = reuse ):
+        # First fully connected layer
+        x1 = tf.layers.dense( z, 4 * 4 * size_mult * 4 )
+        # Reshape it to start the convolutional stack
+        x1 = tf.reshape( x1, ( -1, 4, 4, size_mult * 4) )
+        x1 = tf.layers.batch_normalization( x1, training = training )
+        x1 = tf.maximun( alpha * x1, x1 )
+
+        x2 = tf.layers.conv2d_transpose( x1, size_mult * 2, 5, strides = 2, padding = 'same' )
+        x2 = tf.layers.batch_normalization( x2, training = training )
+        x2 = tf.maximun( alpha * x2, x2 )
+        
+        x3 = tf.layers.conv2d_transpose( x2, size_mult, 5, strides = 2, padding = 'same' )
+        x3 = tf.layers.batch_normalization( x3, training = training )
+        x3 = tf.maximun( alpha * x3, x3 )
+
+        # Out layer
+        logits = tf.layers.conv2d_transpose( x3, output_dim, 5, strides = 2, padding = 'same' )
+
+        out = tf.tanh( logits )
+
+        return out
