5074ef0536850e86a7b61177544a7e5d669ee9f4
Iron Mastiff <1760298283@qq.com>
2018-03-08 18:35:31 +0800

    Finished

--- download_data.py
+++ download_data.py
@@ -7,8 +7,6 @@ import numpy as np
 from scipy.io import loadmat    # 加载mat文件
 import tensorflow as tf
 
-extra_class = 0
-
 from urllib.request import urlretrieve    # 通过url下载数据
 from os.path import isfile, isdir
 from tqdm import tqdm    # 用包装好的方法实现进度条
